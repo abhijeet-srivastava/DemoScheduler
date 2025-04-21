2913724d20164f02a01679f20e170b961d0ab81e
abhijeet.srivastva <malviyanabhijeet@gmail.com>
2025-04-21 11:38:50 +0530

    Inital commit

---
+++ src/test/java/com/narvan/DemoScheduler/DemoSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narvan.DemoScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
